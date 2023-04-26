ada36ac9de015c1617a5739e0940a19a36231c7f
Baole Fang <baole.fang@gmail.com>
2023-04-26 01:07:01 -0400

    feat: fix bug and support embedding MI

---
+++ loss.py
@@ -0,0 +1,54 @@
+# Created by Baole Fang at 4/26/23
+
+from speechbrain.nnet.losses import LogSoftmaxWrapper, AdditiveAngularMargin
+from speechbrain.pretrained import EncoderClassifier
+from data import create_dataloader
+import torch
+import numpy as np
+import os
+
+
+def compute_features(model, loader, criterion):
+    features=[]
+    labels=[]
+    lab2ind=model.hparams.label_encoder.lab2ind
+    for i, data in enumerate(loader):
+        x, label, y = data
+        x = x.cuda()
+        y = torch.tensor([[lab2ind[lab]] for lab in y]).cuda()
+        with torch.inference_mode():
+            pred, score, _, _ = model.classify_batch(x)
+            loss = criterion(pred, y)
+        features.append([loss,score])
+        labels.append(label)
+    return torch.tensor(features).numpy(), torch.tensor(labels).numpy()
+
+
+def pipeline(model_name,dir,root):
+    if os.path.exists(os.path.join(root, 'train.npy')):
+        train = np.load(os.path.join(root, 'train.npy'))
+        val = np.load(os.path.join(root, 'val.npy'))
+        test = np.load(os.path.join(root, 'test.npy'))
+    else:
+        if not os.path.exists(root):
+            os.makedirs(root)
+        criterion = LogSoftmaxWrapper(AdditiveAngularMargin(margin=0.2, scale=30))
+        model = EncoderClassifier.from_hparams(source="speechbrain/"+model_name, run_opts={"device": "cuda"})
+        model.eval()
+        train_loader, val_loader, test_loader = create_dataloader(
+            root=os.path.join('split', dir),
+            data_root='/home/baole/cmu/2023spring/11785/data/vox1/vox1_all/wav/',
+            batch_size=1
+        )
+
+        train_x, train_y = compute_features(model, train_loader, criterion)
+        val_x, val_y = compute_features(model, val_loader, criterion)
+        test_x, test_y = compute_features(model, test_loader, criterion)
+
+        train = np.column_stack((train_x, train_y))
+        np.save(os.path.join(root, 'train.npy'), train)
+        val = np.column_stack((val_x, val_y))
+        np.save(os.path.join(root, 'val.npy'), val)
+        test = np.column_stack((test_x, test_y))
+        np.save(os.path.join(root, 'test.npy'), test)
+    return train,val,test
